ad6a243a5c83df2f07d42a20be81ec4d5e2d8ae7
shiki <kishintorii@gmail.com>
2024-09-04 18:58:07 +0900

    save

---
+++ src/components/Avatar/index.ts
@@ -0,0 +1,11 @@
+import AvatarRoot from './AvatarRoot.astro';
+import AvatarImage from './AvatarImage.astro';
+import AvatarFallback from './AvatarFallback.astro';
+
+const Avatar = {
+    Root: AvatarRoot,
+    Image: AvatarImage,
+    Fallback: AvatarFallback,
+}
+
+export default Avatar;
